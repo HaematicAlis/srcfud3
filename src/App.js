0de80806b78752c7c5833f983458c071cf8a899c
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2023-04-19 03:13:40 -0400

    App setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return <>
+        <h1 style={{color: 'white', marginLeft: 20}}>Hello world</h1>
+    </>;
+};
+
+export default App;
