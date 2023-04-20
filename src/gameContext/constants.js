b52e6743afe4e180fa4e78a0b2869a34376aa715
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2023-04-20 03:11:19 -0400

    Added constants file with maps and enums

---
+++ src/gameContext/constants.js
@@ -0,0 +1,11 @@
+export const enemyTypesEnum = {
+    0: 'melee',
+    1: 'ranged',
+    2: 'magic'
+};
+
+export const enemyColorMap = {
+    'melee': 'red',
+    'ranged': 'green',
+    'magic': 'blue'
+};
