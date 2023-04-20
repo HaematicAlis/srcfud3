5d5be9fab66cd59b8136de9caad546181efdbc95
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2023-04-20 03:12:06 -0400

    Added display for enemies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,33 @@
 import React from 'react';
+import Enemy from './Enemy/Enemy.js';
 import { GameContext } from '../../gameContext';
 import { reducer } from '../../gameContext/reducer.js';
 import { defaultGameState } from '../../gameContext/defaultGameState.js';
 import { useKeyPressCallback } from '../../hooks/keyListenerHook.js';
-import { handleKeyPress } from '../../gameContext/actions.js';
 import './Home.css';
 
 export const doHandleKeyPress = (game, dispatch, keyPressed) => {
     const keyAction = game?.keyBindings?.[game.currentMenu]?.[keyPressed] ?? '';
     if (keyAction) {
-        dispatch(handleKeyPress(keyAction));
+        dispatch({ type: keyAction, parameters: {} });
     }
 };
 
 const Home = () => {
     const innerContext = React.useReducer(reducer, defaultGameState);
-    const [gameContextState, dispatch] = innerContext;
+    const [game, dispatch] = innerContext;
 
     useKeyPressCallback((event) => {
-        doHandleKeyPress(gameContextState, dispatch, event.key);
+        doHandleKeyPress(game, dispatch, event.key);
     }, [dispatch]);
 
     return <>
         <GameContext.Provider value={innerContext}>
-            <h1 className='xpText'>XP: {gameContextState.xp}</h1>
+            <h1 className='xpText'>XP: {game.xp}</h1>
+
+            {game.enemies.map((enemy) => (
+                <Enemy key={enemy.id} enemy={enemy} />
+            ))}
         </GameContext.Provider>
     </>;
 };
