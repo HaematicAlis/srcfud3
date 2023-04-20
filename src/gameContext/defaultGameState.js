92551a6d8f1c829afc025da32d1fd2d4fb9358fe
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2023-04-20 03:00:14 -0400

    Added key binds for each attack type and 3 default enemies

--- src/gameContext/defaultGameState.js
+++ src/gameContext/defaultGameState.js
@@ -3,7 +3,14 @@ export const defaultGameState = {
     currentMenu: 'game',
     keyBindings: {
         'game': {
-            'e': 'attack'
+            'e': 'meleeAttack',
+            'r': 'rangedAttack',
+            't': 'magicAttack'
         }
-    }
+    },
+    enemies: [
+        { id: 0, hp: 1, type: 'melee' },
+        { id: 1, hp: 1, type: 'ranged' },
+        { id: 2, hp: 1, type: 'magic' }
+    ]
 };
