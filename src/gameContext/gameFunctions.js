a274507576f4b3dd150179211b3be916c074b19a
Blaze <47653946+HaematicAlis@users.noreply.github.com>
2023-04-20 03:11:46 -0400

    Removed handleKeyPress and key map and added attack and respawn

--- src/gameContext/gameFunctions.js
+++ src/gameContext/gameFunctions.js
@@ -1,11 +1,29 @@
-export const keyActionMap = {
-    'attack': game => attack(game),
+import { enemyTypesEnum } from './constants.js';
+
+export const attack = (game, attackType) => {
+    const enemy = game.enemies[0] ?? '';
+    if (!enemy) return game;
+
+    if (attackType === enemy.type) {
+        const newEnemies = game.enemies.filter((e) => e.id !== enemy.id);
+        if (newEnemies.length < 1) {
+            return respawn(game);
+        } else {
+            return { ...game, enemies: game.enemies.filter((e) => e.id !== enemy.id)};
+        }
+    } else {
+        return game;
+    }
 };
 
-export const handleKeyPress = (game, keyAction) => {
-    return keyActionMap[keyAction](game);
+export const respawn = (game) => {
+    const type1 = enemyTypesEnum[Math.floor(Math.random() * 3)]; 
+    const type2 = enemyTypesEnum[Math.floor(Math.random() * 3)];
+    const type3 = enemyTypesEnum[Math.floor(Math.random() * 3)];
+    const enemies = [
+        { id: 0, hp: 1, type: type1 },
+        { id: 1, hp: 1, type: type2 },
+        { id: 2, hp: 1, type: type3 }
+    ];
+    return { ...game, enemies: enemies, xp: game.xp + 1 };
 };
-
-export const attack = (game) => {
-    return { ...game, xp: game.xp + 1 };
-}
